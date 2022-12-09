50d50731a4554bfc59735b6e6bcbb2c7f8f338c3
Kirk Hauck <kirkihauck@gmail.com>
2022-12-08 18:57:59 -0700

    Show form page when 'make your own cover' button is clicked

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+var homePage = document.querySelector('.home-view');
+var formPage = document.querySelector('.form-view');
+var savedCoversPage = document.querySelector('.saved-view');
+var homePageButton = document.querySelector('.home-button');
+var formPageButton = document.querySelector('.make-new-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var randomBookButton = document.querySelector('.random-cover-button');
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var coverTagline1 = document.querySelector('.tagline-1');
 var coverTagline2 = document.querySelector('.tagline-2');
-var randomBookButton = document.querySelector('.random-cover-button')
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -10,6 +16,7 @@ var currentCover;
 
 window.addEventListener('load', randomizeBook);
 randomBookButton.addEventListener('click', randomizeBook);
+formPageButton.addEventListener('click', showFormPage);
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -26,4 +33,12 @@ function randomizeBook() {
   coverTitle.innerText = currentCover.title;
   coverTagline1.innerText = currentCover.tagline1;
   coverTagline2.innerText = currentCover.tagline2;
+}
+
+function showFormPage() {
+  homePage.classList.add('hidden');
+  randomBookButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  formPage.classList.remove('hidden');
+  homePageButton.classList.remove('hidden');
 }
